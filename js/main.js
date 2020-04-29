81258737f278c1e26bb53b5398cb58ae469286d2
ahsadek93 <62098165+ahsadek93@users.noreply.github.com>
2020-04-29 04:26:57 +0200

    delying vue to onload

--- js/main.js
+++ js/main.js
@@ -25,62 +25,65 @@ var p = new Vue({
 
 });
 
+window.onload = function () {
 
-var v = new Vue({
-    el: '#section_gallery',
-    data() {
-        return {
-            activeID: 'btn-0',
-            catagoryShown: {
-                cata0: true,
-                cata1: true,
-                cata2: true,
-                cata3: true,
-            }
+    var v = new Vue({
+        el: '#section_gallery',
+        data() {
+            return {
+                activeID: 'btn-0',
+                catagoryShown: {
+                    cata0: true,
+                    cata1: true,
+                    cata2: true,
+                    cata3: true,
+                }
 
-        }
-    },
-    methods: {
-        getActiveClass(id) {
-            if (id === this.activeID) {
-                return 'btn btn-outline-primary border-primary m-2 btn-active'
-            } else {
-                return 'btn btn-outline-primary border-primary m-2'
             }
         },
+        methods: {
+            getActiveClass(id) {
+                if (id === this.activeID) {
+                    return 'btn btn-outline-primary border-primary m-2 btn-active'
+                } else {
+                    return 'btn btn-outline-primary border-primary m-2'
+                }
+            },
 
-        filterCata() {
+            filterCata() {
 
-            if (this.activeID === 'btn-0') {
-                this.catagoryShown.cata0 = true;
-                this.catagoryShown.cata1 = true;
-                this.catagoryShown.cata2 = true;
-                this.catagoryShown.cata3 = true;
+                if (this.activeID === 'btn-0') {
+                    this.catagoryShown.cata0 = true;
+                    this.catagoryShown.cata1 = true;
+                    this.catagoryShown.cata2 = true;
+                    this.catagoryShown.cata3 = true;
 
-            } else if (this.activeID === 'btn-1') {
-                this.catagoryShown.cata0 = true;
-                this.catagoryShown.cata1 = false;
-                this.catagoryShown.cata2 = false;
-                this.catagoryShown.cata3 = false;
+                } else if (this.activeID === 'btn-1') {
+                    this.catagoryShown.cata0 = true;
+                    this.catagoryShown.cata1 = false;
+                    this.catagoryShown.cata2 = false;
+                    this.catagoryShown.cata3 = false;
 
-            } else if (this.activeID === 'btn-2') {
-                this.catagoryShown.cata0 = false;
-                this.catagoryShown.cata1 = true;
-                this.catagoryShown.cata2 = false;
-                this.catagoryShown.cata3 = false;
+                } else if (this.activeID === 'btn-2') {
+                    this.catagoryShown.cata0 = false;
+                    this.catagoryShown.cata1 = true;
+                    this.catagoryShown.cata2 = false;
+                    this.catagoryShown.cata3 = false;
 
-            } else if (this.activeID === 'btn-3') {
-                this.catagoryShown.cata0 = false;
-                this.catagoryShown.cata1 = false;
-                this.catagoryShown.cata2 = true;
-                this.catagoryShown.cata3 = false;
+                } else if (this.activeID === 'btn-3') {
+                    this.catagoryShown.cata0 = false;
+                    this.catagoryShown.cata1 = false;
+                    this.catagoryShown.cata2 = true;
+                    this.catagoryShown.cata3 = false;
 
-            } else if (this.activeID === 'btn-4') {
-                this.catagoryShown.cata0 = false;
-                this.catagoryShown.cata1 = false;
-                this.catagoryShown.cata2 = false;
-                this.catagoryShown.cata3 = true;
+                } else if (this.activeID === 'btn-4') {
+                    this.catagoryShown.cata0 = false;
+                    this.catagoryShown.cata1 = false;
+                    this.catagoryShown.cata2 = false;
+                    this.catagoryShown.cata3 = true;
+                }
             }
         }
-    }
-});
+    });
+
+}
