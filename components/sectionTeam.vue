3b9efc140868e587f4477ad65ecada8f6111a0f3
Ahmed Sadek <62098165+ahsadek93@users.noreply.github.com>
2020-07-18 18:46:58 +0200

    removed docs and general formmating

--- components/sectionTeam.vue
+++ components/sectionTeam.vue
@@ -1,35 +1,35 @@
 <template>
-    <div class="row">
-            <section class="bg-primary col-12 text-center py-5" id="section_team">
-                    <h1 class="text-white  wow fadeInDown">Team</h1>
-                    <p class="text-padding text-white  wow fadeInDown">Lorem ipsum dolor sit amet, consectetur
-                        adipisicing
-                        elit. Quod, nam
-                        corporis
-                        quas, saepe minima error aperiam dolorum aliquam, quis deserunt eos eius quisquam odio itaque.
-                    </p>
-                    <div class="row justify-content-center">
-                        <div class="col-10 col-sm-6 col-md-2 m-4  wow bounceIn">
-                            <img              src="./../assets/t01.webp"
-
-                                class="img-fluid">
-                            <h6 class="text-white my-2">Chris Williams</h6>
-                            <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                        </div>
-                        <div class="col-10 col-sm-6 col-md-2 m-4 wow bounceIn">
-                            <img src="./../assets/t02.webp"
-                                class="img-fluid">
-                            <h6 class="text-white my-2">Randy Dode</h6>
-                            <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                        </div>
-                        <div class="col-10 col-sm-6 col-md-2 m-4 wow bounceIn">
-                            <img src="./../assets/t03.webp"
-                                class="img-fluid">
-                            <h6 class="text-white my-2">Mitchel Kemet</h6>
-                            <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                        </div>
-                    </div>
-
-                </section>
-    </div>
+  <div class="row">
+    <section class="bg-primary col-12 text-center py-5" id="section_team">
+      <h1 class="text-white wow fadeInDown">Team</h1>
+      <p class="text-padding text-white wow fadeInDown">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, nam
+        corporis quas, saepe minima error aperiam dolorum aliquam, quis deserunt
+        eos eius quisquam odio itaque.
+      </p>
+      <div class="row justify-content-center">
+        <div class="col-10 col-sm-6 col-md-2 m-4 wow bounceIn">
+          <img src="./../assets/t01.webp" class="img-fluid" />
+          <h6 class="text-white my-2">Chris Williams</h6>
+          <p class="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </p>
+        </div>
+        <div class="col-10 col-sm-6 col-md-2 m-4 wow bounceIn">
+          <img src="./../assets/t02.webp" class="img-fluid" />
+          <h6 class="text-white my-2">Randy Dode</h6>
+          <p class="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </p>
+        </div>
+        <div class="col-10 col-sm-6 col-md-2 m-4 wow bounceIn">
+          <img src="./../assets/t03.webp" class="img-fluid" />
+          <h6 class="text-white my-2">Mitchel Kemet</h6>
+          <p class="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
